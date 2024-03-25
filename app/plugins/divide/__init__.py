219db42b29715a40dd8aa18c9cbe3c8bd401491e
aravind0815 <as4588@njit.edu>
2024-03-24 22:42:08 -0400

    division file created

---
+++ app/plugins/divide/__init__.py
@@ -0,0 +1,7 @@
+from app.commands import Command
+
+class AddCommand(Command):
+    def execute(self):
+        input1 = float(input("Enter the first number: "))
+        input2 = float(input("Enter the second number: "))
+        print(input1 / input2)

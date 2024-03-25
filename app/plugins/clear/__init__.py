ee8f1306aee81de38e5c9dc524578db3c55ab4e4
aravind0815 <as4588@njit.edu>
2024-03-25 00:45:01 -0400

    Clear folder created to clear the history of operations

---
+++ app/plugins/clear/__init__.py
@@ -0,0 +1,11 @@
+import sys
+from app.commands import Command
+from app.history import History
+import logging
+
+
+class ClearCommand(Command):
+    def execute(self):
+        hist_inst = History()
+        hist_inst.clear()
+        logging.info('Calculator history has been cleared')

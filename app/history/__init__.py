350e1845eceb5d7a884d304fed733b32df33a71a
aravind0815 <as4588@njit.edu>
2024-03-25 23:09:11 -0400

    code change done

--- app/history/__init__.py
+++ app/history/__init__.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 class History:
     def __init__(self):
-        path= os.environ.get('calc_file_path')
+        path= os.environ.get('CALC_FILE_PATH')
         self.path = path
         self.counter = 1
         if not os.path.exists(os.path.dirname(path)):
